5a34efd7a08940ac476892144e963b0d619892fd
Asher Pappas <asherpappas@gmail.com>
2021-01-22 20:02:52 -0700

    Get weather function added

--- script.js
+++ script.js
@@ -16,5 +16,30 @@ function initPage() {
 // Personal API key 
 const APIKey = "d4294475a91f8a0c51e1020ccf4305ad";
 
+// Get Weather function for Current and Forecast 
+function getWeather(cityName) {
+    let queryURL= "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIKey;
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response){
+    // Uses the Date constructor to return the date timestamp * 1000 ms
+        const currentDate = new Date(response.dt*1000);
+        const day = currentDate.getDate();
+    // Returns the month of the year in numerical value of 0-11, so add 1
+        const month = currentDate.getMonth() + 1;
+        const year = currentDate.getFullYear();
+        nameEl.innerHTML = response.name + " (" + month + "/" + day "/" + year + ") ";
+    // Calls the weather icon object
+        let weatherPic = response.weather[0].icon;
+        currentPicEl.setAttribute("src","https://openweathermap.org/img/wn/" + weatherPic + "@2x.png");
+        currentPicEl.setAttribute("alt",response.weather[0].description);
+     // Calls the temp, humidity, and wind speed objects
+        currentTempEl.innerHTML = "Temperature: " + k2f(response.main.temp) + "&#176F";
+        currentHumidityEl.innerHTML = "Humidity: " + response.main.humidity + "%";
+        currentWindEl.innerHTML = "Wind Speed: " + response.wind.speed + " MPH";
 
+      // Getting longitude and latitude for the one-call-api
+    })
+}
 }
